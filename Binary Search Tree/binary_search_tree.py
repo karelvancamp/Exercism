97ab0cb9397f1611c69e88be00c33e8d76774352
Karel Van Camp <karel.vancamp@gmail.com>
2018-08-13 17:02:53 +0200

    Alphanumeric sort

--- Binary Search Tree/binary_search_tree.py
+++ Binary Search Tree/binary_search_tree.py
@@ -1,19 +1,16 @@
 class TreeNode(object):
     def __init__(self, data, left=None, right=None):
-        self.data = str(data)
+        self.data = data
         self.left = left
         self.right = right
 
 class BinarySearchTree(object):
-    def __init__(self, tree_data):
-        
-        intx = list(map(int,tree_data))
-        self.tree = TreeNode(intx[0])
-        
-        for x in intx[1:]:
+    def __init__(self, tree_data): 
+        self.tree = TreeNode(tree_data[0])
+        for x in tree_data[1:]:
             node = self.tree
             while True:
-                if int(node.data) >= x:
+                if node.data >= x:
                     if node.left:
                         node = node.left
                     else:
